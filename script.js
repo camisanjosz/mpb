442554d5ea1b327ee92669c20bd28ff1a12e5fc3
camisanjosz <camila.araujo.anjos@escola.pr.gov.br>
2025-06-24 11:34:16 +0000

    aula 4

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener("click", mostraPalavrasChave);
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
+
 
 function mostraPalavrasChave() {
-  const texto = document.querySelector("#entrada-de-texto").value;
-
-  const campoResultado = document.querySelector("#resultado-palavrachave");
-
-  const palavras = texto.split(" ");
+  const texto = document.querySelector('#entrada-de-texto').value;
+  const campoResultado = document.querySelector('#resultado-palavrachave');
+  const palavrasChave = processaTexto(texto);
 
   campoResultado.textContent = palavras;
   
   campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto) {
+  let palavras = texto.split(" ");
+
+  return palavras;
 }
