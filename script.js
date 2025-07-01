f125768bb51530abb1eb54c22a44ee65b9d51845
camisanjosz <camila.araujo.anjos@escola.pr.gov.br>
2025-07-01 10:59:31 +0000

    aula5

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ function mostraPalavrasChave() {
   const texto = document.querySelector('#entrada-de-texto').value;
   const campoResultado = document.querySelector('#resultado-palavrachave');
   const palavrasChave = processaTexto(texto);
-
-  campoResultado.textContent = palavras;
   
   campoResultado.textContent = palavras.join(", ");
 }
@@ -17,5 +15,16 @@ function processaTexto(texto)
 {
   let palavras = texto.split(/\P{L}+/u);
 
-  return palavras;
+  let frequencias = {};
+    for (let i of palavras) {
+        frequencias[i] = 0;
+        for (let j of palavras) {
+            if (i == j) {
+                frequencias[i]++;
+              }
+          }
+      }      
+      console.log(frequencias);
+
+    return palavras;
 }
