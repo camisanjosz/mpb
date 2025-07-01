b53fd099415d5bb1b8536ff710aa3d9d796b37a1
camisanjosz <camila.araujo.anjos@escola.pr.gov.br>
2025-07-01 11:07:03 +0000

    aula 6

--- script.js
+++ script.js
@@ -2,29 +2,36 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
-
 function mostraPalavrasChave() {
-  const texto = document.querySelector('#entrada-de-texto').value;
-  const campoResultado = document.querySelector('#resultado-palavrachave');
-  const palavrasChave = processaTexto(texto);
-  
-  campoResultado.textContent = palavras.join(", ");
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
 }
 
-function processaTexto(texto) 
-{
-  let palavras = texto.split(/\P{L}+/u);
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias).sort(ordenaPalavra);
 
-  let frequencias = {};
+    function ordenaPalavra(p1, p2) {
+        return frequencias[p2] - frequencias[p1];
+    }
+
+    console.log(ordenadas);
+    return ordenadas.slice(0, 10);
+}
+
+function contaFrequencias(palavras) {
+    let frequencias = {};
     for (let i of palavras) {
         frequencias[i] = 0;
         for (let j of palavras) {
             if (i == j) {
                 frequencias[i]++;
-              }
-          }
-      }      
-      console.log(frequencias);
-
-    return palavras;
+            }
+        }
+    }
+    return frequencias;
 }
